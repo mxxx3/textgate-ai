e37d352d357666bb8044bd1aed616df843329a99
Mariusz K. <mxxxx31@gmail.com>
2026-09-20 00:42:53 +0100

    Fix setup guide lint errors and  version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.textgate.ai"
         minSdk = 26
         targetSdk = 36
-        versionCode = 39
-        versionName = "2.0.11"
+        versionCode = 40
+        versionName = "2.0.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
